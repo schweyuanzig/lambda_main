e8a343eccbc760be2ce6574b06300c50f736c279
schweyuanzig <2012source@gmail.com>
2026-03-07 23:18:45 +0300

    .

--- meg.lua
+++ meg.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/d3fdc5a85f5a5fd27d2124943ccf6317ce5329dfbc0bed5fa53771c5d0d0e892/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/36f123e5bdf13bd8e14ac335c465da614ada4021b289dea6796a0eaaf1751436/download"))()
